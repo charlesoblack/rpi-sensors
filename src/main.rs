a9224ca29210eafe06081cbcbfd91e6287fd5345
Guido Petri <18634426+guidopetri@users.noreply.github.com>
2021-12-31 15:00:26 -0500

    print ack in rust

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,18 @@
+use gpio::{GpioIn, GpioOut};
+use std::{thread, time};
+
 fn main() {
-    println!("Hello, world!");
+    let mut data_pin = gpio::sysfs::SysFsGpioOutput::open(36).unwrap();
+
+    data_pin.set_value(false).expect("could not set init value false");
+    thread::sleep(time::Duration::from_millis(20));
+    data_pin.set_value(true).expect("could not set init value true");
+    thread::sleep(time::Duration::from_micros(30));
+
+    let mut data_pin = gpio::sysfs::SysFsGpioInput::open(36).unwrap();
+
+    loop {
+        println!("GPIO 36: {:?}", data_pin.read_value().unwrap());
+        thread::sleep(time::Duration::from_micros(1));
+    }
 }
